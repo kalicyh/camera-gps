7ed62628d38c68cd83abdab08a4faae1f428bb3f
Saschl <noreply@saschl.com>
2025-08-18 12:21:22 +0200

    fix logviewer status bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.saschl.cameragps"
         minSdk = 31
         targetSdk = 36
-        versionCode = 26
-        versionName = "v1.0.23"
+        versionCode = 27
+        versionName = "v1.0.24"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
