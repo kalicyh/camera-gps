a0e6d3a982da3faa193ec3e6a9b98649952da616
Sascha Rudolf <sascha.rudolf@printus.de>
2025-08-20 15:00:52 +0200

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.saschl.cameragps"
         minSdk = 31
         targetSdk = 36
-        versionCode = 36
-        versionName = "v1.0.36"
+        versionCode = 37
+        versionName = "v1.0.37"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
